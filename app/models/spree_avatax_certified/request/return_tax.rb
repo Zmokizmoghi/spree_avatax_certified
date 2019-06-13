924bcce1dce16de7bec9354558e0992ce29e20df
Alexander Gornov <zmokizmoghi@gmail.com>
2019-06-13 18:11:32 +0800

    fix up returns again

--- app/models/spree_avatax_certified/request/return_tax.rb
+++ app/models/spree_avatax_certified/request/return_tax.rb
@@ -9,10 +9,17 @@ class SpreeAvataxCertified::Request::ReturnTax < SpreeAvataxCertified::Request::
     @request = {
       refundTransactionCode: "C#{Rails.env.production ? 'R' : 'S'}#{order.number.gsub(/[^0-9]/, '')}",
       refundDate: Date.today.strftime('%F'),
-      refundType: 'Full',
+      refundType: 'Partial',
       referenceCode: "Refund for a committed transaction #{order.number}",
+      refundLines: sales_lines
     }
 
     @request
+  end
+
+  protected
+
+  def sales_lines
+    @sales_lines ||= SpreeAvataxCertified::Line.new(order, @doc_type, @refund).lines
   end
 end
